4dff5d58a2030e0c00e92b2e3d215948d104b65d
Lucio Asciolla <93661365+brancolupi@users.noreply.github.com>
2024-03-13 14:17:44 +0100

    Update custom_breadcrumbs.php

--- custom_breadcrumbs.php
+++ custom_breadcrumbs.php
@@ -19,55 +19,54 @@ function the_breadcrumb() {
 
         echo $starter;
 	
-	// Check if the current page is a category, an archive or a single page. If so show the category or archive name.
     // Controlla se la pagina corrente è una categoria, un archivio o una pagina singola. In tal caso mostra la categoria o il nome dell'archivio.
-        // if (is_category() || is_single() ){
-        //     the_category('title_li=');
-        // } elseif (is_archive() || is_single()){
-        //     if ( is_day() ) {
-        //         printf( __( '%s', 'text_domain' ), get_the_date() );
-        //     } elseif ( is_month() ) {
-        //         printf( __( '%s', 'text_domain' ), get_the_date( _x( 'F Y', 'monthly archives date format', 'text_domain' ) ) );
-        //     } elseif ( is_year() ) {
-        //         printf( __( '%s', 'text_domain' ), get_the_date( _x( 'Y', 'yearly archives date format', 'text_domain' ) ) );
-        //     } else {
-        //         _e( 'Blog Archives', 'text_domain' );
-        //     }
-        // }
-	
-	// Se la pagina corrente è la visualizzazione di un singolo post, mostra la categoria di appartenzenza ed il suo titolo com separatori
-        if (is_single()){
+        if (is_category() || is_single() ){
 
             echo '&nbsp;'.$separator.'&nbsp;';
             the_category('title_li=');
 
+        } elseif (is_archive() || is_single()){
+
+            if ( is_day() ) {
+                printf( __( '%s', 'text_domain' ), get_the_date() );
+            } elseif ( is_month() ) {
+                printf( __( '%s', 'text_domain' ), get_the_date( _x( 'F Y', 'formato della data degli archivi mensili', 'text_domain' ) ) );
+            } elseif ( is_year() ) {
+                printf( __( '%s', 'text_domain' ), get_the_date( _x( 'Y', 'formato della data degli archivi annuali', 'text_domain' ) ) );
+            } else {
+                _e( 'Archivio', 'text_domain' );
+            }
+        }
+	
+	// Se la pagina corrente è la visualizzazione di un singolo post, mostra il separatore ed il suo titolo del post (si concatena con "is_archive")
+        if (is_single()){
+
             the_title( '&nbsp;'.$separator.'&nbsp;', '', true );
         }
 	
-	// Se la pagina corrente è una pagina statica, mostra il suo titolo con separatore
+	// Se la pagina corrente è una pagina statica, mostra il separatore ed il titolo della pagina
         if (is_page()){
 
              the_title( '&nbsp;'.$separator.'&nbsp;', '', true );
 
         }
 	
-	// if you have a static page assigned to be you posts list page. It will find the title of the static page and display it. i.e Home >> Blog
-        // if (is_home()){
-        //     global $post;
-        //     $page_for_posts_id = get_option('page_for_posts');
-        //     if ( $page_for_posts_id ) { 
-        //         $post = get_post($page_for_posts_id);
-        //         setup_postdata($post);
-        //         the_title();
-        //         rewind_posts();
-        //     }
+    // Se hai una pagina assegnata come pagina di elenco dei tuoi post. Troverà il titolo della pagina statica e lo visualizzerà. Es. Home >> Blog
+        if (is_home()){
+            global $post;
+            $page_for_posts_id = get_option('page_for_posts');
+            if ( $page_for_posts_id ) { 
+                $post = get_post($page_for_posts_id);
+                setup_postdata($post);
+                the_title( '&nbsp;'.$separator.'&nbsp;', '', true );
+                rewind_posts();
+            }
 
-        // }
+        }
 
         echo '</p></div>';
 
     }
-
 
 // Richiamare la funzione all'interno delle pagine desiderate in questo modo
 

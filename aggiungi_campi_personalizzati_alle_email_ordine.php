979616a05fa1d9d3fa15c01b47691d825347e3e6
Lucio Asciolla <93661365+brancolupi@users.noreply.github.com>
2024-04-03 10:49:17 +0200

    Create aggiungi_campi_personalizzati_alle_email_ordine.php

---
+++ aggiungi_campi_personalizzati_alle_email_ordine.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Add a custom field (in an order) to the emails
+ */
+add_filter( 'woocommerce_email_order_meta_fields', 'custom_woocommerce_email_order_meta_fields', 10, 3 );
+
+function custom_woocommerce_email_order_meta_fields( $fields, $sent_to_admin, $order ) {
+   
+			$fields['partita_iva'] = array(
+			'label' => __( 'Partita IVA:' ),
+			'value' => get_post_meta($order->id, 'Partita IVA', true ),
+			);
+			
+			$fields['codice_sdi'] = array(
+			'label' => __( 'SDI:' ),
+			'value' => get_post_meta($order->id, 'SDI', true ),
+			);
+			
+			$fields['codice_fiscale'] = array(
+			'label' => __( 'Codice fiscale:' ),
+			'value' => get_post_meta( $order->id, 'Codice fiscale', true ),
+			);
+			
+			$fields['indirizzo_pec'] = array(
+			'label' => __( 'Indirizzo PEC:' ),
+			'value' => get_post_meta($order->id, 'Indirizzo PEC', true ),
+			);
+	
+    return $fields;
+}
+
+?>

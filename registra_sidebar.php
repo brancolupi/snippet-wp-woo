4d9d776cb1612bca893fb7b16e57e28631570c5f
Lucio Asciolla <93661365+brancolupi@users.noreply.github.com>
2024-04-03 11:01:52 +0200

    Create registra_sidebar.php

---
+++ registra_sidebar.php
@@ -0,0 +1,20 @@
+<?php
+
+function register_mobile_shop_sidebar() {
+	register_sidebar( array(
+		'name'          => __( 'Mobile Shop Sidebar', 'textdomain' ),
+		'id'            => 'custom-sidebar',
+		'description'   => __( 'I widget in questa sidebar sono mostrati solo nella pagina catalogo-prodotti.', 'textdomain' ),
+		'before_widget' => '<section style="display:flex;">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h1>',
+		'after_title'   => '</h1>',
+	) );
+}
+
+add_action( 'widgets_init', 'register_mobile_shop_sidebar' );
+
+?>
+
+<!-- Utilizzo -->
+<?php dynamic_sidebar('custom-sidebar'); ?>

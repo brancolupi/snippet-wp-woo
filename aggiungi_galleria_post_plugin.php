7bac64d6d6c5d1145cf06158826d41f84809b57a
Lucio Asciolla <93661365+brancolupi@users.noreply.github.com>
2024-04-04 17:31:12 +0200

    Update aggiungi_galleria_post_plugin.php

--- aggiungi_galleria_post_plugin.php
+++ aggiungi_galleria_post_plugin.php
@@ -7,7 +7,7 @@
  * Author:            Lucio Asciolla Full Stack Web Developer
  * Author URI:        https://www.cinquepuntozero.it
  * Cellulare:	      +393477085217
- * Email:	      lucio.asciolla@gmail.com
+ * Email:	          lucio.asciolla@gmail.com
  * License:           GPL v2 or later
  * License URI:       HTTPS://WWW.GNU.ORG/LICENSES/GPL-2.0.HTML
  * Update URI:        HTTPS://EXAMPLE.COM/MY-PLUGIN/
@@ -15,6 +15,46 @@
  * Domain Path:       /languages
  */
 ?>
+
+<?php 
+
+    ///////////// START Abilitare $_FILES aggiungendo "multipart/form-data" al form dei post ///////////////
+
+    if(is_admin()){
+    $current_admin_page = substr(strrchr($_SERVER['PHP_SELF'], '/'), 1, -4);
+        
+        if($current_admin_page == 'page' || $current_admin_page == 'page-new' || $current_admin_page == 'post' || $current_admin_page == 'post-new'){
+
+            function aggiungi_post_enctype(){
+                echo "<script type=\"text/javascript\">
+                document.addEventListener('DOMContentLoaded', function() {
+                var postForm = document.getElementById('post');
+                postForm.setAttribute('enctype', 'multipart/form-data');
+                postForm.setAttribute('encoding', 'multipart/form-data');
+                });
+                </script>";
+            }
+
+        add_action('admin_head', 'aggiungi_post_enctype');
+            
+        }
+    }
+
+    ///////////// END Abilitare $_FILES aggiungendo "multipart/form-data" al form dei post ///////////////
+
+    /////////// START Abilitare BOOTSTAP CSS Framework  C/CDN nella font-end e nel backend ///////////////
+
+    function abilita_bootstrap_css_framework() {
+        wp_enqueue_style( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css' );
+    }
+
+    add_action('wp_enqueue_scripts', 'abilita_bootstrap_css_framework'); // Front-end
+    add_action('admin_enqueue_scripts', 'abilita_bootstrap_css_framework'); // Back-end
+    
+    /////////// START Abilitare BOOTSTAP CSS Framework  C/CDN nella font-end e nel backend ///////////////
+
+?>
+
 
 <?php
 
@@ -35,10 +75,9 @@ add_action('add_meta_boxes', 'crea_gallery_metabox');
 function aggiungi_contenuto_interfaccia_metabox_gallery(){ 
 
 global $post; 
-
-$__image_gallery = get_post_meta( $post->ID, '_image_gallery', true);
               
 ?> 
+
 
 <!-- start metabox container -->
 <div class="container-fluid m-0 p-0"> 
@@ -53,51 +92,120 @@ $__image_gallery = get_post_meta( $post->ID, '_image_gallery', true);
 			</div>	
 		</div>	
        
-		<!-- start load image section -->
+		<!-- START Caricamento immagini box -->
 				
 		<div class="row align-items-center justify-content-center"> 
+			
+			<!--  START Recupero dati -->
+			<div>
+			
+			<?php 
+				$_esistenti_image_loaders_da_salvare = get_post_meta( $post->ID, '_esistenti_image_loaders_da_salvare', true);
+				//var_dump( $_esistenti_image_loaders_da_salvare );
+
+				$_esistenti_image_loaders_urls = [];	
+				foreach($_esistenti_image_loaders_da_salvare as $_esistenti_image_loader){
+				$_esistenti_image_loaders_urls[] = get_post_meta($post->ID, $_esistenti_image_loader, true);
+                // echo $_esistenti_image_loader;
+                // echo get_post_meta($post->ID, $_esistenti_image_loader, true);
+				}
+				//var_dump($_esistenti_image_loaders_urls);
+			?>
+				
+			<input 
+				type="text" 
+				id="_esistenti_image_loaders_da_salvare" 
+				name="_esistenti_image_loaders_da_salvare" 
+				style="width:100%;"
+				value="<?php echo implode(',', get_post_meta($post->ID, '_esistenti_image_loaders_da_salvare', true)); ?>">
+				
+			<input 
+				type="text" 
+				id="_esistenti_image_loaders_da_salvare_urls" 
+				name="_esistenti_image_loaders_da_salvare_urls" 
+   				style="width:100%;"
+				value="<?php echo implode(',', $_esistenti_image_loaders_urls); ?>">
+			
+			</div>
+			
+			<!--  END Recupero dati -->
+			
+			
 			<div id="container_box_image_load" class="row mt-2 mb-2">
-				<div image_loader_number="1" class="col-2 image_loader" style="position:relative;">
-					<p 
-					   class="image_remove_button" 
-					   style="position: absolute; top: -2%; right: 0%; background: #BA2F2F; padding: 1% 5%; border-radius: 5px; color: white; cursor:pointer;" 
-					   onclick="removeImage(1)"
-					>Rimuovi</p>
-					<p 
-					   class="image_delete_button" 
-					   style="position: absolute; top: 7%; right: 0%; background: #BA2F2F; padding: 1% 5%; border-radius: 5px; color: white; cursor:pointer;" 
-					   onclick="deleteImage(1)"
-					>Elimina</p>
-					<img 
-						 id="image_view_1" 
-						 src="<?php if( get_post_meta( $post->ID, 'image_gallery_1', false) ){ echo get_post_meta( $post->ID, 'image_gallery_1', false); }else{ echo '/wp-content/uploads/2024/03/image_placeholder.jpg'; } ?>" style="width: 100%;"
-						 >
-					<br>
-					<div class="row mt-2 mb-2 ms-2">
-						<input 
-							   id="image_load_input_1" 
-							   type="file" 
-							   name="_image_gallery_1" 
-							   value="" 
-							   onchange="letturaFile(1)">
-					</div>
+				
+				<!-- START Lavorazione dati e compilazione -->
+				
+				<script>
 					
+				var _esistenti_image_loaders_da_salvare = document.getElementById('_esistenti_image_loaders_da_salvare').value;
+					
+					_esistenti_image_loaders_da_salvare_Array_Strs =  _esistenti_image_loaders_da_salvare.split(',');
+					
+					_esistenti_image_loaders_da_salvare_Array_Ids = [];
+					_esistenti_image_loaders_da_salvare_Array_Strs.forEach(function(image_loader){
+								_esistenti_image_loaders_da_salvare_Array_Ids.push(image_loader.substring(15));	
+					})
+					
+				var _esistenti_image_loaders_da_salvare_urls = document.getElementById('_esistenti_image_loaders_da_salvare_urls').value;
+					esistenti_image_loaders_da_salvare_urls_Array_Strs =  _esistenti_image_loaders_da_salvare_urls.split(',');
+
+				console.log(_esistenti_image_loaders_da_salvare_Array_Strs);
+				console.log(_esistenti_image_loaders_da_salvare_Array_Ids);
+					
+				var container_box_image_load = document.getElementById('container_box_image_load');
+				var container_box_image_load_content = '';
+				var _esistenti_image_loaders_da_salvare_Array_Ids_length = _esistenti_image_loaders_da_salvare_Array_Ids.length;
+					
+				for(var x=1; x < _esistenti_image_loaders_da_salvare_Array_Ids_length; x++){
+					container_box_image_load_content += `
+					<div image_loader_number="${_esistenti_image_loaders_da_salvare_Array_Ids[x]}" class="col-2 image_loader" style="position:relative;">
+						<p 
+						   class="image_delete_button" 
+						   style="position: absolute; top: -2%; right: 0%; background: #BA2F2F; padding: 1% 5%; border-radius: 5px; color: white; cursor:pointer;" 
+						   onclick="deleteImage(${_esistenti_image_loaders_da_salvare_Array_Ids[x]})"
+						>Elimina</p>
+						<img 
+							 id="image_view_${_esistenti_image_loaders_da_salvare_Array_Ids[x]}" 
+							 src="${esistenti_image_loaders_da_salvare_urls_Array_Strs[x]}" style="width: 100%;"
+							 >
+						<br>
+						<div class="row mt-2 mb-2 ms-2">
+							<input 
+								   id="image_load_input_${_esistenti_image_loaders_da_salvare_Array_Ids[x]}" 
+								   type="file" 
+								   name="_image_gallery_${_esistenti_image_loaders_da_salvare_Array_Ids[x]}" 
+								   value="" 
+								   onchange="letturaFile(${_esistenti_image_loaders_da_salvare_Array_Ids[x]})">
+						</div></div>`;
+				}
+
+				//console.log(container_box_image_load_content);	
+				
+				container_box_image_load.innerHTML = container_box_image_load_content;
+					
+				</script>
+				
+				<!-- END Lavorazione dati e compilazione -->
+
+				
 					<script>
 						
-						var _esistenti_image_loaders_da_salvare = [];
+						var _esistenti_image_loaders_da_salvare = (document.getElementById('_esistenti_image_loaders_da_salvare').value).split(',');
 
 						function aggiungi_inputs_name_da_salvare(number){
 							var campoFile = document.getElementById(`image_load_input_${number}`);
-							_esistenti_image_loaders_da_salvare.push(campoFile.getAttribute('name'));
-							document.getElementById('_esistenti_image_loaders_da_salvare').value = JSON.stringify(_esistenti_image_loaders_da_salvare);
-						}
+                            if(!_esistenti_image_loaders_da_salvare.includes(campoFile.getAttribute('name'))){
+                                _esistenti_image_loaders_da_salvare.push(campoFile.getAttribute('name'));
+                            }
+   							document.getElementById('_esistenti_image_loaders_da_salvare').value = _esistenti_image_loaders_da_salvare.join(',');
+                        }
 
 						function rimuovi_inputs_name_da_salvare(number){
 							var campoFile = document.getElementById(`image_load_input_${number}`);
 							var nameCampoFile = campoFile.getAttribute('name');
 							var indexCampoFile = _esistenti_image_loaders_da_salvare.indexOf(nameCampoFile);
 							_esistenti_image_loaders_da_salvare.splice(indexCampoFile, 1);
-							document.getElementById('_esistenti_image_loaders_da_salvare').value = JSON.stringify(_esistenti_image_loaders_da_salvare);
+							document.getElementById('_esistenti_image_loaders_da_salvare').value = _esistenti_image_loaders_da_salvare.join(',');
 						}
 						
 						function letturaFile(number){
@@ -115,11 +223,6 @@ $__image_gallery = get_post_meta( $post->ID, '_image_gallery', true);
 							aggiungi_inputs_name_da_salvare(number);
 						}	
 						
-						function removeImage(number){
-							document.getElementById(`image_view_${number}`).setAttribute("src", "/wp-content/uploads/2024/03/image_placeholder.jpg");
-							document.getElementById(`image_load_input_${number}`).setAttribute("value", "");
-						}
-						
 						function deleteImage(number){
 							rimuovi_inputs_name_da_salvare(number);
 							document.querySelector(`div[image_loader_number="${number}"]`).remove();
@@ -132,7 +235,7 @@ $__image_gallery = get_post_meta( $post->ID, '_image_gallery', true);
 
         </div> 
 			
-		<!-- end load load section -->
+		<!-- END Caricamento immagini box -->
 			
 		<script>
 					
@@ -141,7 +244,12 @@ $__image_gallery = get_post_meta( $post->ID, '_image_gallery', true);
 			
 			all_image_loader_number = document.querySelectorAll('.image_loader');
 			number_of_image_loader_number = all_image_loader_number.length;
-			counter_last_image_loader = all_image_loader_number[number_of_image_loader_number-1].getAttribute('image_loader_number');
+            if(number_of_image_loader_number > 0 ){
+                counter_last_image_loader = all_image_loader_number[number_of_image_loader_number-1].getAttribute('image_loader_number');
+
+            }else{
+                counter_last_image_loader = 0;
+            }
 
 			console.log(Number(counter_last_image_loader));
 			
@@ -159,13 +267,8 @@ $__image_gallery = get_post_meta( $post->ID, '_image_gallery', true);
 				nuovo_load_box.setAttribute('image_loader_number', Number(counter_last_image_loader) + 1);
 				nuovo_load_box.innerHTML= `
 					<p 
-					   class="image_remove_button" 
-					   style="position: absolute; top: -2%; right: 0%; background: #BA2F2F; padding: 1% 5%; border-radius: 5px; color: white; cursor:pointer;" 
-					   onclick="removeImage( ${Number(counter_last_image_loader) + 1} );"
-					   >Rimuovi</p>
-					<p 
 					   class="image_delete_button" 
-					   style="position: absolute; top: 7%; right: 0%; background: #BA2F2F; padding: 1% 5%; border-radius: 5px; color: white; cursor:pointer;" 
+					   style="position: absolute; top: -2%; right: 0%; background: #BA2F2F; padding: 1% 5%; border-radius: 5px; color: white; cursor:pointer;" 
 					   onclick="deleteImage( ${Number(counter_last_image_loader) + 1} )"
 						>Elimina</p>
 					<img id="image_view_${Number(counter_last_image_loader) + 1}" src="/wp-content/uploads/2024/03/image_placeholder.jpg" 
@@ -189,19 +292,12 @@ $__image_gallery = get_post_meta( $post->ID, '_image_gallery', true);
         <div class="row mt-3"> 
             <div class="col-12">
             <p style="color:gray; font-size:0.65rem;">
-            post_type: "post"<br>
-            meta_key: "_esistenti_image_loaders_da_salvare" (array serializzato)<br>
+				<strong>post_type:</strong> "post"<br>
+				<strong>meta_key:</strong> "_esistenti_image_loaders_da_salvare" (array serializzato)<br>
+				<strong>meta_value:</strong> <?php echo implode(', ', $_esistenti_image_loaders_da_salvare); ?>
             </p>
             </div>
         </div> 
-	
-		<div>
-		<input 
-			   type="hidden" 
-			   id="_esistenti_image_loaders_da_salvare" 
-			   name="_esistenti_image_loaders_da_salvare" 
-			   value="<?php echo implode(',', get_post_meta($post->ID, '_esistenti_image_loaders_da_salvare', true)); ?>">
-		</div>
 
 	
 </div> 
@@ -222,54 +318,33 @@ if(isset( $_POST["_esistenti_image_loaders_da_salvare"] )){
 	$_convert_array_from_string_js = explode( ',', str_replace( '"', '', str_replace( '[', '', str_replace(']', '', $_POST['_esistenti_image_loaders_da_salvare'] ) ) ) );
 	update_post_meta( $post->ID, '_esistenti_image_loaders_da_salvare', $_convert_array_from_string_js );
 
-	foreach($_FILES as $file){
-		foreach($_convert_array_from_string_js as $loader){
-			move_uploaded_file( $file['tmp_name'], $_SERVER['DOCUMENT_ROOT'] . '/wp-content/themes/nikolaos/assets/img/post/' . $file['name'] );
-			update_post_meta( $post->ID, $loader, '/wp-content/themes/nikolaos/assets/img/post/' . $file['name'] );
-		}
-	}
+	// foreach($_FILES as $file){
+	// 	foreach($_convert_array_from_string_js as $loader){
+	// 		if($file['tmp_name'] != ''){
+	// 			move_uploaded_file( $file['tmp_name'], $_SERVER['DOCUMENT_ROOT'] . '/wp-content/themes/nikolaos/assets/img/post/' . $file['name'] );
+	// 			update_post_meta( $post->ID, $loader, '/wp-content/themes/nikolaos/assets/img/post/' . $file['name'] );
+	// 		}
+	// 	}
+	// }
+
+
+    $counter_files = 0;
+
+    foreach($_FILES as $file){
+
+            if($file['tmp_name'] != ''){
+                move_uploaded_file( $file['tmp_name'], $_SERVER['DOCUMENT_ROOT'] . '/wp-content/themes/nikolaos/assets/img/post/' . $file['name'] );
+                update_post_meta( $post->ID, $_convert_array_from_string_js[$counter_files], '/wp-content/themes/nikolaos/assets/img/post/' . $file['name'] );
+            }
+        
+            $counter_files++;
+
+    }
 
 }
 	
 }
-          
+
 add_action('save_post', 'salva_dati_gallery_metabox');
-
-?>
-
-
-<?php 
-
-///////////// START Abilitare $_FILES aggiungendo "multipart/form-data" al form dei post ///////////////
-if(is_admin()){
-$current_admin_page = substr(strrchr($_SERVER['PHP_SELF'], '/'), 1, -4);
-	
-	if($current_admin_page == 'page' || $current_admin_page == 'page-new' || $current_admin_page == 'post' || $current_admin_page == 'post-new'){
-
-		function aggiungi_post_enctype(){
-			echo "<script type=\"text/javascript\">
-			document.addEventListener('DOMContentLoaded', function() {
-			  var postForm = document.getElementById('post');
-			  postForm.setAttribute('enctype', 'multipart/form-data');
-			  postForm.setAttribute('encoding', 'multipart/form-data');
-			});
-			</script>";
-		}
-
-	add_action('admin_head', 'aggiungi_post_enctype');
-		
-	}
-}
-///////////// END Abilitare $_FILES aggiungendo "multipart/form-data" al form dei post ///////////////
-
-/////////// START Abilitare BOOTSTAP CSS Framework  C/CDN nella font-end e nel backend ///////////////
-function abilita_bootstrap_css_framework() {
-    wp_enqueue_style( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css' );
-}
-
-add_action('wp_enqueue_scripts', 'abilita_bootstrap_css_framework'); // Front-end
-add_action('admin_enqueue_scripts', 'abilita_bootstrap_css_framework'); // Back-end
-   
-/////////// START Abilitare BOOTSTAP CSS Framework  C/CDN nella font-end e nel backend ///////////////
 
 ?>

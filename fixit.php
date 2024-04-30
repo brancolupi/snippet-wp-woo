4abf6f6d13690bffabb0fc492839fbc1391c9c98
Lucio Asciolla <93661365+brancolupi@users.noreply.github.com>
2024-04-30 10:22:15 +0200

    Update fixit.php

--- fixit.php
+++ fixit.php
@@ -9,5 +9,14 @@ define('WP_TEMP_DIR', dirname(__FILE__) . '/wp-content/temp/');
 // Modificare i permessi in "775" della directory /wp-content
 // Potrebbe essere necessario effettuare un refesh della versione PHP
 
+////////////////////////// Aggiungere funzionalità wordpress a file (ajax) *.php esterno //////////////////////////
+
+define('WP_USE_THEMES', false);
+require_once($_SERVER['DOCUMENT_ROOT'].'/wp-load.php');
+
+global $woocommerce;
+global $wordpress;
+
+
 ?>
 

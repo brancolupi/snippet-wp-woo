af1eee2d60fabccddb7ecc1edbf3e437a24a14ee
Lucio Asciolla <93661365+brancolupi@users.noreply.github.com>
2024-05-08 11:21:31 +0200

    Update aggiungi_ruoli_iutente.php

--- aggiungi_ruoli_iutente.php
+++ aggiungi_ruoli_iutente.php
@@ -1,18 +1,19 @@
-<?php
+<?php 
 
 ///////////////// Create user's roles ///////////////
+// https://developer.wordpress.org/reference/functions/add_role/
+// https://developer.wordpress.org/reference/functions/current_user_can/
 
-add_role( 'Risorsa', 'risorsa_capability', array('read'=> true) );
+add_role( 'Rivenditore', 'rivenditore_capability', array('read'=> true) );
 
 function custom_hide_admin_bar() {
     // Verifica il ruolo dell'utente attuale
-    if ( current_user_can('risorsa_capability') ) {
+    if ( current_user_can('rivenditore_capability') ) {
         // Nascondi la barra di amministrazione per gli utenti con il ruolo "Subscriber"
         show_admin_bar(false);
     }
 }
+
 add_action('after_setup_theme', 'custom_hide_admin_bar');
-
-
 
 ?>
